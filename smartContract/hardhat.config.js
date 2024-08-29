1e63a0d081b12e07a26dfb004bfc07dc17827c1e
kanakkadulkar <144000040+kanakkadulkar@users.noreply.github.com>
2024-08-29 18:55:16 +0530

    smart contract init

---
+++ smartContract/hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+	solidity: {
+		version: "0.8.4",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 100,
+			},
+		},
+	},
+
+	// networks: {
+	// 	polygon_mumbai: {
+	// 		url: process.env.POLYGON_TESTNET_URL,
+	// 		accounts: [process.env.PRIVATE_KEY],
+	// 	},
+	// },
+	// etherscan: {
+	// 	apiKey: {
+	// 		polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+	// 	},
+	// },
+};
