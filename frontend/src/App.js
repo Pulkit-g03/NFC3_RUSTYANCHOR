3b7f4a47c58d812bd77b4344435e2734f01b6bd5
kanakkadulkar <144000040+kanakkadulkar@users.noreply.github.com>
2024-08-29 16:49:51 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<div className=" overflow-hidden">
-			<h1>Hello World </h1>
+			<Header/>
+			<Footer/>
 		</div>
 	);
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+	
+
+	return (
+		<header className="bg-gray-900 text-lg">
+			<div className="flex justify-between items-center px-4">
+				<Link to="/" className="text-blue text-2xl font-bold cursor-pointer">
+					Dog Suraksha
+				</Link>
+				<ul className="flex space-x-7">
+					<li>
+						<Link
+							to="/quoteForm"
+							className="text-white hover:text-gray-300 cursor-pointer"
+						>
+							Get a Quote
+						</Link>
+					</li>
+
+					<li>
+						<Link
+							to="/claim"
+							className="text-white hover:text-gray-300 cursor-pointer"
+						>
+							Claim policy
+						</Link>
+					</li>
+
+					<li>
+						<Link
+							to="/admin"
+							className="text-white hover:text-gray-300 cursor-pointer"
+						>
+							Admin
+						</Link>
+					</li>
+
+					
+					<li>
+						<Link
+							to="/howToInstructions"
+							className="text-white hover:text-gray-300 cursor-pointer"
+						>
+							How-to Instructions
+						</Link>
+					</li>
+				</ul>
+				
+			</div>
+		</header>
+	);
+};
+
+export default Header;
